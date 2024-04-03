2c10dfaa2382c10eb59b5f13fb5bcdd32920bf13
He Qian Wang <heqian.wang@gsoft.com>
2024-04-03 16:07:09 -0400

    Add unique Id to version name

--- Build.ps1
+++ Build.ps1
@@ -25,7 +25,8 @@ Process {
     Exec { & dotnet tool restore }
 
     # Let GitVersion compute the NuGet package version
-    $version = Exec { & dotnet dotnet-gitversion /output json /showvariable SemVer }
+    $uniqueId = Get-Date -Format "yyyyMMddHHmmss"
+    $version = Exec { & dotnet dotnet-gitversion /output json /showvariable SemVer } + ".$uniqueId"
 
     # Pack using NuGet.exe
     Exec { & nuget pack Workleap.DotNet.CodingStandards.nuspec -OutputDirectory $outputDir -Version $version -ForceEnglishOutput }
@@ -35,7 +36,7 @@ Process {
 
     # Push to a NuGet feed if the environment variables are set
     if (($null -ne $env:NUGET_SOURCE ) -and ($null -ne $env:NUGET_API_KEY)) {
-      Exec { & dotnet nuget push "$nupkgsPath" -s $env:NUGET_SOURCE -k $env:NUGET_API_KEY --skip-duplicate }
+      Exec { & dotnet nuget push "$nupkgsPath" -s $env:NUGET_SOURCE -k $env:NUGET_API_KEY }
     }
   }
   finally {
